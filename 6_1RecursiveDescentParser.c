68af1d59c8e80b5bd976a5c0e2869a92e66c0aa8
Ashmitha P R <ashmitha.pr04@gmail.com>
2025-10-27 21:18:23 +0530

    6_1RecursiveDescentParser.c

--- 6_1RecursiveDescentParser.c
+++ 6_1RecursiveDescentParser.c
@@ -1,5 +1,72 @@
 #include<stdio.h>
-#include<stdlib.h>
-/*
-    
-*/
+
+void E();
+void T();
+void Eprime();
+void Tprime();
+void F();
+
+int error = 0;
+char input[30];
+int i=0;
+
+int main(){
+	printf("Grammar : \nE->TE'\nE'->+TE'|e\nT->FT'\nT'->*FT'|e\nF->(E)|id\n");
+	printf("Enter the input (must end with $) : ");
+	scanf("%s",input);
+	E();
+	if(error == 0 && input[i] == '$'){
+		printf("Valid");
+	}
+	else{
+		printf("Invalid");
+	}
+}
+
+void E(){
+	T();
+	Eprime();
+}
+
+void Eprime(){
+	if(input[i] == '+'){
+		i++;
+		T();
+		Eprime();
+	}
+}
+
+void T(){
+	F();
+	Tprime();
+}
+
+void Tprime(){
+	if(input[i] == '*'){
+		i++;
+		F();
+		Tprime();
+	}
+}
+
+void F(){
+	if(input[i] == 'i' && input[i+1] == 'd'){
+		i+=2;
+	}
+	else{
+		if(input[i] == '(' ){
+			i++;
+			E();
+			if(input[i] == ')'){
+				i++;
+			}
+			else{
+				error = 1;
+			}
+		}
+		else{
+			error = 1;
+		}
+	}
+}
+
